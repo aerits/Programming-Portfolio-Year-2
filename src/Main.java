728e0292a1c48bb9f71a9b7a9393bd10e522e051
aerits <stev_nm@protonmail.com>
2022-08-23 02:40:40 +0000

    we do a little calculatin

--- src/Main.java
+++ src/Main.java
@@ -3,46 +3,67 @@ import java.util.Scanner;
 class Main {
 
   public static void main(String[] args) {
-    System.out.print("\033[H\033[2J");
-    System.out.flush();
+    // loops the code if you enter an invalid operator
+    for (int x = 0; x < 1;) {
+      // clears the screen
+      System.out.print("\033[H\033[2J");
+      System.out.flush();
 
-    Scanner myObj = new Scanner(System.in);
-    System.out.println("calculator");
+      // welcome user, create thing to get input
+      Scanner scan = new Scanner(System.in);
+      System.out.println("it\'s calculatin\' time");
+      System.out.print("type in an expression: ");
 
-    String[] inputs = { "", "" };
-    String input = myObj.nextLine();
-    myObj.close();
+      // get input
+      String[] inputs = { "", "" };
+      String input = scan.nextLine();
 
-    String operator = "";
+      // initialize variables
+      String operator = "";
 
-    String operators[] = { "+", "-", "*", "/" };
-    String operators1[] = { "\\+", "-", "\\*", "/" };
+      String operators[] = { "+", "-", "*", "/" };
+      String operators1[] = { "\\+", "-", "\\*", "/" };
 
-    for (int i = 0; i < 4; i++) {
-      if (input.contains(operators[i])) {
-        inputs = input.split(operators1[i], 2);
-        operator = operators[i];
-        break;
+      // split up input into the first input and second input, also figure out what
+      // operator the user wants to use
+      for (int i = 0; i < 4; i++) {
+        if (input.contains(operators[i])) {
+          inputs = input.split(operators1[i], 2);
+          operator = operators[i];
+          break;
+        }
       }
+
+      // initialize variable
+      int output = 0;
+
+      // increment variable to end for loop
+      x++;
+
+      // commit math
+      switch (operator) {
+        default:
+          // go back into the for loop because invalid operator
+          System.out.println("please enter a valid operator");
+          input = scan.nextLine();
+          x = 0;
+          break;
+        case "+":
+          output = Integer.parseInt(inputs[0]) + Integer.parseInt(inputs[1]);
+          break;
+        case "-":
+          output = Integer.parseInt(inputs[0]) - Integer.parseInt(inputs[1]);
+          break;
+        case "*":
+          output = Integer.parseInt(inputs[0]) * Integer.parseInt(inputs[1]);
+          break;
+        case "/":
+          output = Integer.parseInt(inputs[0]) / Integer.parseInt(inputs[1]);
+          break;
+      }
+
+      // print out the result to the expression
+      System.out.println(output);
     }
-
-    int output = 0;
-
-    switch (operator) {
-      case "+":
-        output = Integer.parseInt(inputs[0]) + Integer.parseInt(inputs[1]);
-        break;
-      case "-":
-        output = Integer.parseInt(inputs[0]) - Integer.parseInt(inputs[1]);
-        break;
-      case "*":
-        output = Integer.parseInt(inputs[0]) * Integer.parseInt(inputs[1]);
-        break;
-      case "/":
-        output = Integer.parseInt(inputs[0]) / Integer.parseInt(inputs[1]);
-        break;
-    }
-
-    System.out.println(output);
   }
 }
