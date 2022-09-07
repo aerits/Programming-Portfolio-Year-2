8bc8fa0294a2604dc02b6bee1deb97d685586aca
aerits <stev_nm@protonmail.com>
2022-09-07 14:09:01 +0000

    dfa

---
+++ src/hungman.java
@@ -0,0 +1,24 @@
+public class hungman {
+    public static void main(String[] args) {
+        int percent=80;
+        int bruh = (percent/(100/7));
+        System.out.println(bruh);
+        switch (bruh) {
+            case 7:
+                System.out.print("---  ");
+            case 6:
+                System.out.print(" o");
+            case 5:
+                System.out.print("\n/");
+            case 4:
+                System.out.print("|");
+            case 3:
+                System.out.print("\\");
+            case 2:
+                System.out.print("\n/");
+            case 1:
+                System.out.print(" \\\n");
+                break;
+        }
+    }
+}
